633d2de034809be5787235c5988dfe3a6b9cb5cd
Burak Katı <burkinggs@gmail.com>
2023-04-28 19:11:47 +0300

    Add files via upload

---
+++ Nodejs/Odev3/circle.js
@@ -0,0 +1,10 @@
+const circleArea = (r) => {
+
+    console.log(Math.PI * r * r);
+}
+
+const circleCircumference = (r) => {
+    console.log(Math.PI * 2 * r);
+}
+
+module.exports = { circleArea, circleCircumference };
